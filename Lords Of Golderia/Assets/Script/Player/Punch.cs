08af7c1f49f08df1914acd62ebb78f4656ab0a3b
Loïc <loic.disarbois@ynov.com>
2020-11-04 17:11:33 +0100

    Punch V1

--- Lords Of Golderia/Assets/Script/Player/Punch.cs
+++ Lords Of Golderia/Assets/Script/Player/Punch.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Punch : MonoBehaviour
 {
+
+    public bool punching = false;
+
+    public Vector3 orgPos;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -16,5 +22,30 @@ public class Punch : MonoBehaviour
         
     }
 
-    
+    public void OnPunch(InputAction.CallbackContext context)
+    {
+        if (!punching)
+        {
+            punching = true;
+            StartCoroutine(ActionPunch(0.25f, 0.80f, transform.forward));
+        }
+    }
+
+  
+
+    IEnumerator ActionPunch(float time, float distance, Vector3 direction)
+    {
+        var timer = 0.0f;
+        orgPos = gameObject.transform.position;
+        direction.Normalize();
+        while (timer <= time)
+        {
+            var oe2ouf = orgPos + (Mathf.Sin(timer / time * Mathf.PI) + distance) * direction;
+            transform.position = oe2ouf.normalized;
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        gameObject.transform.position = orgPos;
+        punching = false;
+    }
 }
